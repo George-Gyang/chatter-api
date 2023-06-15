91957fb5d5d5f28044e7b20c228f635b6d1dcb95
George-Gyang <georgegyang3@gmail.com>
2023-06-15 08:42:29 +0100

    implement hashSnch to password

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const express = require("express");
-const cors = require('cors')
-const mongoose = require("mongoose")
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const User = require('./models/user')
+const User = require("./models/user");
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const app = express();
 const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // mongodb connection
-mongoose.connect('mongodb+srv://chatter:thUEvbnjj2VcVaEE@cluster0.kaymlzu.mongodb.net/?retryWrites=true&w=majority')
-
+mongoose.connect(
+  "mongodb+srv://chatter:thUEvbnjj2VcVaEE@cluster0.kaymlzu.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.post("/register", async (req, res) => {
-    const {firstName, lastName, email, password, confirmPassword} = req.body;
-    try{
-      const userInfor = await User.create({firstName, lastName, email, password, confirmPassword})
+  const { email, firstName, lastName, password, confirmPassword } = req.body;
+  try {
+    const userInfor = await User.create({
+      email,
+      firstName,
+      lastName,
+      password: bcrypt.hashSync(password, salt),
+      confirmPassword: bcrypt.hashSync(confirmPassword, salt),
+    });
     res.json(userInfor);
-    } catch(e){
-      res.status(400).json(e)
-    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 });
+
+// app.post("/login", (req, res) => {
+//   const { email, password } = req.body;
+//   User.findOne(email);
+// });
 
 app.listen(4000);
--- models/User.js
+++ models/User.js
@@ -2,14 +2,22 @@ const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const UserSchema = new Schema({
-  firstName: { type: String, require: true, unique: true },
+const UserSchema = new Schema({ 
+  email: { type: String, require: true, unique: true },
+  firstName: { type: String, require: true, },
   lastName: { type: String, require: true },
-  email: { type: String, require: true },
   password: { type: String, require: true },
-  confirmPassword: { type: String, require: true },
+  confirmPassword: {
+    type: String,
+    require: true,
+    validate: {
+      validator: function(value){
+        return value === this.password;
+      }, message: 'password not  match'
+    }
+  },
 });
 
-const UserModel = model('User', UserSchema);
+const UserModel = model("User", UserSchema);
 
 module.exports = UserModel;
